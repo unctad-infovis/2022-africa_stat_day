eec7d6b86b76223259cf19ac9c56aeeaaded27e1
Teemo Tebest <teemo.tebest@gmail.com>
2022-11-18 13:30:58 +0100

    Initial commit

---
+++ src/jsx/Figure1.jsx
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from 'react';
+
+// Load helpers.
+import CSVtoJSON from './helpers/CSVtoJSON.js';
+import ChartStackedLine from './components/ChartStackedLine.jsx';
+
+import '../styles/styles.less';
+
+function Figure1() {
+  // Data states.
+  const [dataFigure, setDataFigure] = useState(false);
+
+  const cleanData = (data) => data.map((el, i) => {
+    const labels = Object.keys(el).filter(val => val !== 'Name').map(val => parseInt(val, 10));
+    const values = Object.values(el).map(val => (parseFloat(val))).filter(val => !Number.isNaN(val));
+
+    return ({
+      data: values.map((e, j) => ({
+        x: labels[j],
+        y: e
+      })),
+      zoneAxis: 'x',
+      zones: [{
+        value: 2022
+      }, {
+        fillColor: (i === 1) ? 'rgba(114, 191, 68, 0.4)' : 'rgba(0, 158, 219, 0.4)'
+      }],
+      name: el.Name
+    });
+  });
+
+  useEffect(() => {
+    const data_file = `${(window.location.href.includes('unctad.org')) ? 'https://storage.unctad.org/2022-africa_stat_day/' : './'}assets/data/2022-africa_stat_day_figure_1.csv`;
+    try {
+      fetch(data_file)
+        .then((response) => {
+          if (!response.ok) {
+            throw Error(response.statusText);
+          }
+          return response.text();
+        })
+        .then(body => setDataFigure(cleanData(CSVtoJSON(body))));
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+  return (
+    <div className="app">
+      {dataFigure && (
+      <ChartStackedLine
+        data={dataFigure}
+        data_decimals={0}
+        idx="1"
+        note="The graph provides estimates from 1950 to 2021 and projections from 2022 to 2050 of total population"
+        source="UNCTADstat based on UN DESA Population Division, World Population Prospects 2022."
+        subtitle="People living in developing and developed economies, billions"
+        suffix=""
+        title="World population growth is mostly in developing economies"
+        ylabel=""
+      />
+      )}
+      <noscript>Your browser does not support JavaScript!</noscript>
+    </div>
+  );
+}
+
+export default Figure1;
